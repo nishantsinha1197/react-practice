da2278d098e8ad18ceb900a5c741e91550043006
NISHANT SINHA <nishantsinha1197@gmail.com>
2024-04-05 18:16:34 +0530

    modified

--- src/components/MultiPage/PageNotFound.js
+++ src/components/MultiPage/PageNotFound.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useLocation} from 'react-router-dom'
 function PageNotFound() {
     let location = useLocation()
-    console.log('kki',location);
+    console.log('location of the page',location);
   return (
     <div>
       <h1>Sorry the {location.pathname} page does not exist</h1>
